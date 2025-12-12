20a7f238fefdcaabd846584543600c51197d074b
dweg0 <dweg0epic@outlook.com>
2025-12-12 11:22:34 -0300

    feat: UrTimerContext has been created to set timer in a std env

--- urust-fw-wroom32d/src/ur_context.rs
+++ urust-fw-wroom32d/src/ur_context.rs
@@ -1,6 +1,8 @@
 use crate::modules::ur_pins_context::UrPinsContext;
 use esp_idf_hal::gpio::OutputPin;
+use crate::modules::ur_timer_context::UrTimerContext;
 
 pub struct UrContext<PIN: OutputPin> {
-    pub led: UrPinsContext<PIN>,
+    pub pin: UrPinsContext<PIN>,
+    pub timer: UrTimerContext,
 }
